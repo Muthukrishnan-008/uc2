ca6a3f57d1e2c6fc53513c229dd2d47b70a84275
Muthukrishnan-008 <mm0761@srmist.edu.in>
2026-03-17 13:17:00 +0530

    Add files via upload

---
+++ UC2.java
@@ -0,0 +1,11 @@
+class UC2_OOPS_Banner {
+    public static void main(String[] args) {
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * * * * *  ***** ");
+        System.out.println("*     * *     * *             *");
+        System.out.println("*     * *     * *       *     *");
+        System.out.println(" *****   *****  *        ***** ");
+    }
+}
